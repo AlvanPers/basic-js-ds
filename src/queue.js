f444dce72a91a367055444b7eac782934996d6f0
Alex <alexyoup@outlook.com>
2023-12-25 23:36:37 +0300

    feat: solution queue..

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
   }
+  getUnderlyingList() {
+    return this.head;
+  }
+  isEmpty() {
+    return this.head === null;
+  }
+  enqueue(value) {
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const node = new ListNode(value);
+
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.size += 1;
+    return this;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.isEmpty()) return null;
+    const elementToDelete = this.head;
+
+    if (this.head === this.tail) {
+      this.tail = null;
+      this.head = null;
+      this.size -= 1;
+      return elementToDelete.value;
+    }
+    this.head = this.head.next;
+    this.size -= 1;
+    return elementToDelete.value;
   }
 }
 
