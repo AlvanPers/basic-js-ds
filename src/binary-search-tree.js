45c273bd1dcebfd29d89f45a4de4c79bfd10b574
Alex <alexyoup@outlook.com>
2023-12-25 23:45:36 +0300

    feat: del comments

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -19,19 +19,15 @@ class BinarySearchTree {
     this.rootEl = addData(this.rootEl, data);
 
     function addData(node, data) {
-      // If there is no any node (null), add new node
       if (!node) {
         return new Node(data);
       }
-      // If such node has already exists
       if (node.data === data) {
         return node;
       }
-      // If new data is less than existing node (go to left)
       if (data < node.data) {
         node.left = addData(node.left, data);
       } else {
-        // If existing node is less than new data (move to right)
         node.right = addData(node.right, data);
       }
       return node;
@@ -42,19 +38,15 @@ class BinarySearchTree {
     return hasData(this.rootEl, data);
 
     function hasData(node, data) {
-      // If there is no such node (null)
       if (!node) {
         return false;
       }
-      // If data is found
       if (node.data === data) {
         return true;
       }
-      // If new data is less than existing node (move to left)
       if (data < node.data) {
         return hasData(node.left, data);
       } else {
-        // If existing node is less than new data (move to right)
         return hasData(node.right, data);
       }
     }
@@ -64,19 +56,15 @@ class BinarySearchTree {
     return findData(this.rootEl, data);
 
     function findData(node, data) {
-      // If there is no such node (null)
       if (!node) {
         return null;
       }
-      // If data is found
       if (node.data === data) {
         return node;
       }
-      // If new data is less than existing node (move to left)
       if (data < node.data) {
         return findData(node.left, data);
       } else {
-        // If existing node is less than new data (move to right)
         return findData(node.right, data);
       }
     }
@@ -85,49 +73,38 @@ class BinarySearchTree {
   remove(data) {
     this.rootEl = removeData(this.rootEl, data);
     function removeData(node, data) {
-      // If there is no node
+
       if (!node) {
         return null;
       }
-      // If data is less that existing node
+
       if (data < node.data) {
         node.left = removeData(node.left, data);
         return node;
       } else if (node.data < data) {
-        // If existing node is less that data 
         node.right = removeData(node.right, data);
         return node;
       } else {
-        // Data is found, remove it. Check if left and right nodes are leaves (means that there are no children)
         if (!node.left && !node.right) {
-          // put instead of data
           return null;
         }
         if (!node.left) {
-          // If there is right child (no left child)
-          // set right child instead of data
           node = node.right;
           return node;
         }
 
         if (!node.right) {
-          // If there is left child (no right child)
-          // set left child instead of data
           node = node.left;
           return node;
         }
-        // If both children exist for data
-        // (remember that from the right all elements are more then current (data))
-        // start from the root of the right subtree till the last element
+
         let minimumRight = node.right;
         while (minimumRight.left) {
-          // while there is element lower, move lower
+
           minimumRight = minimumRight.left;
         }
-        // set found value to the value of the removed node
         node.data = minimumRight.data;
 
-        // remove from the right the value with minimum value
         node.right = removeData(node.right, minimumRight.data);
 
         return node;
@@ -136,13 +113,13 @@ class BinarySearchTree {
   }
 
   min() {
-    // (remember that values from the left are less that the current element (data))
+
     if (!this.rootEl) {
       return null;
     }
-    // start from root
+
     let node = this.rootEl;
-    // Move to left
+
     while (node.left) {
       node = node.left;
     }
@@ -150,13 +127,13 @@ class BinarySearchTree {
   }
 
   max() {
-    // (remember that values from the right are more that the current element (data))
+
     if (!this.rootEl) {
       return null;
     }
-    // start from root
+
     let node = this.rootEl;
-    // Move to right
+
     while (node.right) {
       node = node.right;
     }
